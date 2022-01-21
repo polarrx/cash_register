17d7bdf410e554922b254de6f8c6ee8b356d1600
Jesús <jesus@P43G-847acacf.localdomain>
2022-01-21 16:54:59 -0400

    Primera subida

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import time
+
+
+# Menu de opciones
+def print_options():
+
+	print('\nCAJA REGISTRADORA')
+	print('*' * 50)
+	print('Menu de opciones')
+	print('*' * 50)
+	print('[1] Crear un invetario')
+	print('[2] Ver inventario')
+	print('[3] Ver ventas')
+	print('[4] SALIR')
+
+def run(): # Funcion de arranque que ejecuta la Funcion del menu de opciones
+
+	print_options()
+
+	command = str(input())
+	#command = command.upper()
+
+	if command == '1':
+		create_inventary()
+	elif command == '2':
+	    view_inventary()
+	elif command == '3':
+		print('Mira menol esta opcion esta construyendose')
+		run()
+	elif command == '4':
+		print('#' * 50)
+		print('SALIENDO DEL SISTEMA DE INVENTARIO')
+		print('#' * 50)
+		os._exit(1)
+	else:
+		print('Comando inválido, gafo')
+		time.sleep(2)
+		run()
+
+if __name__ == '__main__':
+	run()
---
+++ util/util.py
@@ -0,0 +1,5 @@
+class Factura():
+    def __init__():
+        self.id_producto = id_producto
+        self.cliente = cliente
+        self.
